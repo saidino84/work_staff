e5e04143804a1a5845785b8d117f7e90ead396fb
saidino84 <tylorguy2018@gmail.com>
2022-09-07 14:41:02 -0400

    Adding transference model

---
+++ workhelp/app/db/models/transferences.py
@@ -0,0 +1,27 @@
+class Tranferencia:
+    
+    def __init__(self, 
+                number,
+                date,
+                subtotal,
+                parcents,
+                total,
+                destin,
+                user,
+                description,
+                state=0
+                ):
+        self.number=number
+        self.date=date
+        self.subtotal=subtotal
+        self.parcents=parcents
+        self.total=total
+        self.destin=destin
+        self.user=user
+        self.description=description
+        self.state=state
+
+    def __repr__(self)->str:
+        return f"N:{self.number},"
+def get_tranference_as_list(self):
+    return 
